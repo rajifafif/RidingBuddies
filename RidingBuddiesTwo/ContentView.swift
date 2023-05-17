3c1a41dc8ca9668a1b58fd157aa62ebcaa718ecf
R <rajifafif@gmail.com>
2023-05-17 14:10:11 +0700

    Initial Commit

---
+++ RidingBuddiesTwo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RidingBuddiesTwo
+//
+//  Created by Rajif Afif on 17/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RidingBuddiesTwo/RidingBuddiesTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidingBuddiesTwoApp.swift
+//  RidingBuddiesTwo
+//
+//  Created by Rajif Afif on 17/05/23.
+//
+
+import SwiftUI
+
+@main
+struct RidingBuddiesTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
